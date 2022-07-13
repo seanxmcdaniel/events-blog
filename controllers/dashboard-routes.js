45c90d0e6870086d5aa612a1b0956d2428ebaa99
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-07-13 10:50:27 -0400

    Update dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -38,47 +38,6 @@ router.get('/', (req, res) => {
         })
 })
 
-//get events by id
-router.get('/events/:id', (req, res) => {
-    Event.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'title',
-            'description',
-            'location',
-            'date',
-            'vendor_name',
-            //[sequelize.literal('(SELECT COUNT (*) FROM going WHERE event.id = going.event_id)')]
-        ],
-        include: [
-            {
-                model: Vendor,
-                attributes: ['email']
-            }
-        ]
-    })
-        .then(dbEventData => {
-            if (!dbEventData) {
-                res.status(404).json({ message: 'No event found with this id.' });
-                return;
-            }
-
-            const event = dbEventData.get({ plain: true });
-
-            res.render('single-event', {
-                event,
-                loggedIn: req.session.loggedIn
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
-});
-
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
