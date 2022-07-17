462c9cfb863498ae4372ad72bba1385af9ff6836
Theresa2013 <tab61295@gmail.com>
2022-07-17 15:17:44 -0500

    Add time-stamp package

--- public/js/add-event.js
+++ public/js/add-event.js
@@ -1,31 +1,31 @@
 async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="event-title"]').value;
-    const description = document.querySelector('input[name="event-desc"]').value;
-    const date = document.querySelector('input[name="event-date"]').value;
-    const location = document.querySelector('input[name="event-loc"]').value;
-    const vendorId = document.querySelector('input[name="vendor-id"]').value;
-  
-    const response = await fetch(`/api/events`, {
-      method: 'post',
-      body: JSON.stringify({
-        title,
-        description,
-        date,
-        location,
-        vendor_id: vendorId
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="event-title"]').value;
+  const description = document.querySelector('input[name="event-desc"]').value;
+  const date = document.querySelector('input[name="event-date"]').value;
+  const location = document.querySelector('input[name="event-loc"]').value;
+  const vendorId = document.querySelector('input[name="vendor-id"]').value;
+
+  const response = await fetch(`/api/events`, {
+    method: 'post',
+    body: JSON.stringify({
+      title,
+      description,
+      date,
+      location,
+      vendor_id: vendorId
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.new-event-form').addEventListener('submit', newFormHandler);
+}
+
+document.querySelector('.new-event-form').addEventListener('submit', newFormHandler);
